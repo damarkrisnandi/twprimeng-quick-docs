c3933161b27cc6d9b47bbdcba81c30698c365c5a
damar <damar@gitlab.lab>
2025-07-22 16:53:59 +0700

    update docs

--- src/app/refs/data/login.ts
+++ src/app/refs/data/login.ts
@@ -12,11 +12,12 @@ export const loginComponentData: Doc  = {
 <div class="h-screen flex flex-col justify-center items-center background-layout" [formGroup]="loginForm">
   <div class="w-11/12 md:w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 
-
     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-      <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+      <div class="flex justify-between items-center">
+        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
           Login to your account
-      </h1>
+        </h1>
+      </div>
       <div class="space-y-4 md:space-y-6" action="#">
         <div>
             <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
@@ -48,14 +49,13 @@ export const loginComponentData: Doc  = {
             <button (click)="login()" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</button>
         </div>
         <button type="submit" (click)="login()" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-        <!-- <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
             Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-        </p> -->
+        </p>
     </div>
   </div>
 </div>
 
-<p-toast [breakpoints]="breakpoints" />
 </div>
 `,
     },
